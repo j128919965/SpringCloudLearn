558c6c23ebed4046f17cfb5d8a4b8d328fdb91b2
lzr <853681401@qq.com>
2021-03-13 15:58:43 +0800

    zookeeper

--- payment8004/src/main/java/top/quezr/sc/PaymentMain8004.java
+++ payment8004/src/main/java/top/quezr/sc/PaymentMain8004.java
@@ -1,18 +1,18 @@
-package eureka;
+package top.quezr.sc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author que
  * @version 1.0
- * @date 2021/3/8 21:16
+ * @date 2021/3/6 11:00
  */
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaMain7002 {
+@EnableDiscoveryClient
+public class PaymentMain8004 {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaMain7002.class,args);
+        SpringApplication.run(PaymentMain8004.class,args);
     }
 }
